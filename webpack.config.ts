2c01fe0b6753f42c390439fa1ff798bbed8bd576
Ilia <hikkikomori7@gmail.com>
2025-01-24 23:47:14 +0100

    feature: Local webpack config was replaced by one from submodule

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,31 +1,8 @@
-import * as path from 'path'
-import webpack from 'webpack'
 
-import { buildWebpackConfig } from './config/build/buildWebpackConfig'
-import { BuildEnv } from './config/build/types/config'
+import webpackConfig from '@library/build/webpack.config';
+import { BuildEnv } from '@library/build/config/types/config';
+import path from 'path';
 
 export default (env: BuildEnv) => {
-  const paths = {
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    html: path.resolve(__dirname, 'public', 'index.html'),
-    build: path.resolve(__dirname, 'dist'),
-    src: path.resolve(__dirname, 'src'),
-  }
-
-  const mode = env.mode || 'development'
-  const isDev = mode === 'development'
-  const PORT = env.port || 3000
-  const open = true
-  const project = 'frontend'
-
-  const config: webpack.Configuration = buildWebpackConfig({
-    mode,
-    paths,
-    isDev,
-    port: PORT,
-    open,
-    project,
-  })
-
-  return config
-}
+  return webpackConfig(env, path.resolve(__dirname));
+};
